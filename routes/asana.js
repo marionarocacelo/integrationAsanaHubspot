2436f67bc1b774e11b011485f732bf1dd386fd6f
Mariona Roca <mariona.roca@MacBook-Pro-de-Mariona.local>
2026-03-12 16:04:29 +0100

    ruta test

--- routes/asana.js
+++ routes/asana.js
@@ -7,4 +7,8 @@ const updateDeal = require('../controller/updateDeal');
 /* POST changes in Asana project. */
 router.post('/changes', changedReceived, filter, updateDeal);
 
+router.post('/test', (req, res, next)=>{
+  return res.status(200).json({ message: "ha anat bé!" });
+});
+
 module.exports = router;
